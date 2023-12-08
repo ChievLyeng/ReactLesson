1a9112b822bf203ed5ffcb23281f14c674236aa4
004 <lyeng2210@gmail.com>
2023-12-08 20:14:15 +0700

    8_12

--- Media/src/components/UsersList.jsx
+++ Media/src/components/UsersList.jsx
@@ -1,45 +1,50 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers,addUser } from "../store";
 import Button from './Button';
 import Skeleton from "../components/Skeleton";
 
-function UsersList() {
-    const [isLoadingUsers, setIsLoadingUsers] = useState(false);
-    const [loadingUsersError, setLoadingUsersError] = useState(null);
+function useThunk(thunk) {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
     const dispatch = useDispatch();
 
+    const runThunk = useCallback(() => {
+        setIsLoading(true)
+        dispatch(thunk())
+            .unwrap()
+            .catch((err) => {
+                setError(err)
+            })
+            .finally(() => setIsLoading(false));
+    }, [dispatch,thunk]);
+
+    return [runThunk, isLoading, error];
+}
+
+function UsersList() {
+    const [doFetchUsers, isLoadingUsers, loadingUsersError] = 
+        useThunk(fetchUsers)
+    const [doCreateUser, isCreatingUser, creatingUserError] =
+        useThunk(addUser)
     const { data } = useSelector((state) => {
         return state.users;
     });
 
-    const handlUserAdd = async () => {
-        await dispatch(addUser());
+    const handlUserAdd = () => {
+        doCreateUser();
     }
 
     // Extract data from the Redux state
     const users = data.data?.users || [];
 
     useEffect(() => {
-        setIsLoadingUsers(true)
-        dispatch(fetchUsers())
-            .unwrap()
-            .catch((err) => {
-                console.log("err",err)
-                setLoadingUsersError(err)
-                
-            })
-            .finally(() => {
-                setIsLoadingUsers(false)
-                console.log("err",loadingUsersError)
-            });
-    }, [dispatch]);
+        doFetchUsers();
+    }, [doFetchUsers]);
+
+
 
     
-
-    // console.log("data :", users);
-    // console.log(isLoading);
-    // console.log(error);
 
     if (isLoadingUsers) {
         return <Skeleton times={6} className="h-10 w-full" />;
@@ -75,9 +80,12 @@ function UsersList() {
         <div>
             <div className="flex flex-row justify-between m-3">
                 <h1 className="m-2 text-xl">Users</h1>
-                <Button onClick={handlUserAdd}>
-                    + Add User
-                </Button>
+                {
+                    isCreatingUser ? 'Creating User...' 
+                    : <Button onClick={handlUserAdd}>  + Add User </Button>
+                }
+                
+                {creatingUserError && 'Error creating user...'}
             </div> 
             {renderedUsers} 
         
